7447b09dabe29a30df886461384f927e3f8c0cbe
vengateshm <40466166+vengateshm@users.noreply.github.com>
2022-07-16 19:06:30 +0530

    decrement unread count, unread list item highlight

--- src/main/java/dev/vengateshm/messagingapp/inbox/controllers/EmailViewController.java
+++ src/main/java/dev/vengateshm/messagingapp/inbox/controllers/EmailViewController.java
@@ -1,10 +1,14 @@
 package dev.vengateshm.messagingapp.inbox.controllers;
 
+import dev.vengateshm.messagingapp.inbox.emailList.EmailListItem;
+import dev.vengateshm.messagingapp.inbox.emailList.EmailListItemKey;
+import dev.vengateshm.messagingapp.inbox.emailList.EmailListItemRepository;
 import dev.vengateshm.messagingapp.inbox.emails.Email;
 import dev.vengateshm.messagingapp.inbox.emails.EmailRepository;
 import dev.vengateshm.messagingapp.inbox.folders.Folder;
 import dev.vengateshm.messagingapp.inbox.folders.FolderRepository;
 import dev.vengateshm.messagingapp.inbox.folders.FoldersService;
+import dev.vengateshm.messagingapp.inbox.folders.UnreadEmailStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -13,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,9 +31,14 @@ public class EmailViewController {
     private EmailRepository emailRepository;
     @Autowired
     private FoldersService foldersService;
+    @Autowired
+    private EmailListItemRepository emailListItemRepository;
+    @Autowired
+    private UnreadEmailStatsRepository unreadEmailStatsRepository;
 
     @GetMapping("/emails/{id}")
     public String emailView(
+            @RequestParam String folder,
             @PathVariable String id,
             @AuthenticationPrincipal OAuth2User principal,
             Model model) {
@@ -46,16 +56,33 @@ public class EmailViewController {
         if (userFolders.size() > 0) {
             model.addAttribute("userFolders", userFolders);
         }
-        model.addAttribute("stats", foldersService.mapCountToLabels(userId));
 
         Optional<Email> optionalEmail = emailRepository.findById(UUID.fromString(id));
         if (optionalEmail.isEmpty()) {
             return "inbox-page";
         }
         Email email = optionalEmail.get();
-        String toIds = String.join(", ",email.getTo());
+        String toIds = String.join(", ", email.getTo());
         model.addAttribute("email", email);
         model.addAttribute("toIds", toIds);
+
+        EmailListItemKey key = new EmailListItemKey();
+        key.setId(userId);
+        key.setLabel(folder);
+        key.setTimeUUID(email.getId());
+
+        Optional<EmailListItem> optionalEmailListItem = emailListItemRepository.findById(key);
+        if (optionalEmailListItem.isPresent()) {
+            EmailListItem emailListItem = optionalEmailListItem.get();
+            if (emailListItem.isUnread()) {
+                emailListItem.setUnread(false);
+                emailListItemRepository.save(emailListItem);
+                unreadEmailStatsRepository.decrementUnreadCount(userId, folder);
+            }
+        }
+        // Live decrement when read
+        model.addAttribute("stats", foldersService.mapCountToLabels(userId));
+
         return "email-page";
     }
 }
